fcb42dc49ee7b50818d5321ea0f7da29e36f3620
mkcs4760 <mk4cf@mail.umsl.edu>
2019-04-18 21:29:03 -0500

    Presently creates resource table, sets random numbers to the total resource instances available, and marks roughly 20% of them as sharable resources.

--- oss.c
+++ oss.c
@@ -13,22 +13,40 @@
 #include <errno.h>
 #include <sys/time.h>
 
+int randomNum() {
+	int num = (rand() % (10) + 1);
+	return num;
+}
+
 int main(int argc, char *argv[]) {
+	srand(time(0)); //placed here so we can generate random numbers later on
+	printf("Welcome to project 5\n");
 	
-	int y = 2;
-	int x = 3;
+	int x = 20;
+	int y = 3;
 	int resource[y][x];
 	
 	int i, j;
 	for (i = 0; i < y; i++) {
 		for (j = 0; j < x; j++) {
-			resource[i][j] = 5; //a value
+			if (i == 0) {
+				resource[j][i] = randomNum(); //set the maximum number of this resource available to the system. These numbers do not change
+			} else if ( i == 1) {
+				resource[j][i] = resource[j][0]; //set the current number of available instances of this resource. This number does change
+			} else {
+				if (randomNum() < 3) {
+					resource[j][i] = 1; //mark this resource as a sharable resource
+				} else {
+					resource[j][i] = 0; //mark this resource as a nonsharable resource
+				}
+			}
 		}
 	}
 	
+	//print our resource board
 	for (i = 0; i < y; i++) {
 		for (j = 0; j < x; j++) {
-			printf("%d ", resource[i][j]);
+			printf("%d\t", resource[j][i]);
 		}
 		printf("\n");
 	}
