fa4244d59247c0ff3b60b2df922749fee87215d4
mkcs4760 <mk4cf@mail.umsl.edu>
2019-04-23 15:49:38 -0500

    It more or less works for 2. Waiting for a resource needs to be handled better, so for now it terminates the program, which does NOT release resources naturally. Need to add in deadlock detection algorithm as next step

--- oss.c
+++ oss.c
@@ -18,6 +18,7 @@
 
 #define CLOCK_INC 10000
 #define RESOURCE_COUNT 20
+#define MAX_TIME_BETWEEN_NEW_PROCESSES_NS 500000000
 
 
 struct PCB {
@@ -76,7 +77,7 @@ int main(int argc, char *argv[]) {
 	printf("Welcome to project 5\n");
 	
 	int i, j;
-	int maxKidsAtATime = 1; //for now, this is our test value. This will eventually be up to 18, depending on command line arguments
+	int maxKidsAtATime = 2; //for now, this is our test value. This will eventually be up to 18, depending on command line arguments
 	
 	//we need our process control table
 	struct PCB PCT[maxKidsAtATime]; //here we have a table of maxNum process blocks
@@ -147,40 +148,61 @@ int main(int argc, char *argv[]) {
 	
 	//now let's start our loop, or for this variation, one child called
 	int terminate = 0;
-	int makeChild = 1;
+	//int makeChild = 1;
+	bool newChildPrepped = false;
+	int processesLaunched = 0;
+	int processesRunning = 0;
+	int durationNano, startSeconds, startNano, stopSeconds, stopNano;
 	
 	while (terminate != 1) {
 		//printf("Start of while loop\n");
-		if (makeChild == 1) { //we need to make a child process
-			int openSlot = checkForOpenSlot(PCT, maxKidsAtATime);
-			if (openSlot != -1) { //a return value of -1 means all slots are currently filled, and per instructions we are to ignore this process
-
-				pid_t pid;
-				pid = fork();
+		if (newChildPrepped == false) { //let's prep a new child
+			newChildPrepped = true;
+			durationNano = randomNum(1, MAX_TIME_BETWEEN_NEW_PROCESSES_NS);
+			startSeconds = sm->clockSecs;
+			startNano = sm->clockNano;
+			
+			stopSeconds = startSeconds; //duration seconds is 0, so don't bother making a variable for it
+			stopNano = startNano + durationNano;
+			if (stopNano >= 1000000000) {
+				stopSeconds += 1;
+				stopNano -= 1000000000;
+			}
+			//we will try to launch our next child at stopSeconds:stopNano
+		} else if ((stopSeconds < sm->clockSecs) || ((stopSeconds == sm->clockSecs) && (stopNano < sm->clockNano))) {
+			if (processesLaunched < 3) { //for testing...
 				
-				if (pid == 0) {
-					execl ("user", "user", NULL);
-					errorMessage(programName, "execl function failed. ");
-				}
-				else if (pid > 0) { //parent
-					makeChild = 0; //for now, we only want to create one child, for testing purposes
-					printf("PARENT: Created child %d at %d:%d\n", pid, sm->clockSecs, sm->clockNano);
-					//boolArray[openSlot] = true; //claim that spot
-					//processesLaunched++; //these will be implemented later, so they remain here as a reminder
-					//processesRunning++;
-					//prepNewChild = false;
-					PCT[openSlot].myPID = pid;
-					//printf("Lets set child %d to 0 resources for starting out\n", pid);
-					for (i = 0; i < RESOURCE_COUNT; i++) {
-						PCT[openSlot].myResource[0][i] = 0; //start out with having 0 of each resource
-					} //does the above hunk of code now work???
-					continue;
-				}
-				else {
-					errorMessage(programName, "Unexpected return value from fork ");
+				int openSlot = checkForOpenSlot(PCT, maxKidsAtATime);
+				if (openSlot != -1) { //a return value of -1 means all slots are currently filled, and per instructions we are to ignore this process
+
+					pid_t pid;
+					pid = fork();
+					
+					if (pid == 0) {
+						execl ("user", "user", NULL);
+						errorMessage(programName, "execl function failed. ");
+					}
+					else if (pid > 0) { //parent
+						newChildPrepped = false;
+						printf("PARENT: Created child %d at %d:%d\n", pid, sm->clockSecs, sm->clockNano);
+						processesLaunched++; //these will be implemented later, so they remain here as a reminder
+						processesRunning++;
+						PCT[openSlot].myPID = pid;
+						//printf("Lets set child %d to 0 resources for starting out\n", pid);
+						for (i = 0; i < RESOURCE_COUNT; i++) {
+							PCT[openSlot].myResource[0][i] = 0; //start out with having 0 of each resource
+						} //does the above hunk of code now work???
+						continue;
+					}
+					else {
+						errorMessage(programName, "Unexpected return value from fork ");
+					}
 				}
 			}
+		
 		}
+		
+
 		sm->clockNano += CLOCK_INC; //increment clock
 		if (sm->clockNano >= 1000000000) { //increment the next unit
 			sm->clockSecs += 1;
@@ -221,6 +243,7 @@ int main(int argc, char *argv[]) {
 							break;
 						}
 					}
+					kill(-1*getpid(), SIGKILL);	//just for TESTING ONLY!!!
 				}
 				message.return_address = getpid();
 				int send = msgsnd(msqid, &message, sizeof(message), 0); //send message
@@ -256,7 +279,8 @@ int main(int argc, char *argv[]) {
 		
 		//we check to see if any of our processes have ended
 		int temp = waitpid(-1, NULL, WNOHANG);
-		if (temp < 0) {
+		if ((temp < 0) && (errno != 10)) { //a cheap escape for when there are no processes in the system...
+			printf("Why does waitpid equal %d?\n", temp);
 			errorMessage(programName, "Unexpected result from terminating process ");
 		} else if (temp > 0) {
 			printf("PARENT: Process %d confirmed to have ended at %d:%d\n", temp, sm->clockSecs, sm->clockNano); //SOMETIMES GET A SEGFAULT AFTER THIS...
@@ -279,8 +303,10 @@ int main(int argc, char *argv[]) {
 				}
 			}
 			//printf("PARENT: ending process checkpoint 4\n");
-			//processesRunning--;
-			terminate = 1;
+			processesRunning--;
+			if (processesLaunched > 2 && processesRunning == 0) { //let's see if this works
+				terminate = 1;
+			}
 			//printf("PARENT: ending process checkpoint 5\n");
 		} //if temp == 0, nothing has ended and we simply carry on
 		
@@ -292,15 +318,13 @@ int main(int argc, char *argv[]) {
 			
 			printf("At present, this is what our PCT looks like:\n");
 			for (p = 0; p < maxKidsAtATime; p++) {
-				printf("PARENT0: Slot #%d, containing PID %d: \n", p, PCT[p].myPID);
+				printf("PARENT: Slot #%d, containing PID %d: \n", p, PCT[p].myPID);
 				int q;
 				for (q = 0; q < 20; q++) {
 					printf("%d ", PCT[p].myResource[0][q]);
 				}
 				printf("\n");
 			}
-			
-			printf("\tDDA over\n");
 			
 			/*sprintf("DEADLOCK DETECTION ALGORITHM beginning\n");
 			//copy all data into "simulation" version of them. These we will manipulate without destroying the system data
